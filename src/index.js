ea10957e4c1c12098bb437bcaca91ffed97296d3
Maratib Ali Khan <maratib@gmail.com>
2025-05-18 03:19:51 +0500

    current working path setup

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ const path = require("path");
 const yargs = require("yargs");
 
 if (yargs.argv._[0] == null) {
+
     console.log("No arguments provided");
     return;
 }
 
 if (yargs.argv._[0] === "theme") {
-    const root = path.join(__dirname, '..');
+    const root = process.cwd();
     const themeName = yargs.argv._[1] || "my-theme";
     if (themeName.includes(".") || themeName.includes("/")) {
         console.log("Invalid theme name. Theme name should not contain '.' or '/'");
